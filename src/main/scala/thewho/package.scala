ccf870fadfe5fb4e5353af0dda13844d8c69aa62
Andrés González <a.gonzalez.terres@gmail.com>
2019-05-26 23:33:10 -0300

    Add doobie repository
    Solves: #4

--- src/main/scala/thewho/package.scala
+++ src/main/scala/thewho/package.scala
@@ -5,8 +5,7 @@ import thewho.repository.Repository
 
 package object thewho {
 
-  // TODO #4 live implementation
-  trait TestEnv  extends Repository.Test with Auth.Test with Console.Live with Clock.Live
+  trait TestEnv  extends Repository.Live with Auth.Test with Console.Live with Clock.Live
   object TestEnv extends TestEnv
 
 }
