177651544120056bf2b47f5d4d1f090a0c72771d
Andrés González <a.gonzalez.terres@gmail.com>
2019-06-16 19:14:30 -0300

    Map app errors to http response
    issue: #9

--- src/main/scala/thewho/error/Errors.scala
+++ src/main/scala/thewho/error/Errors.scala
@@ -2,50 +2,37 @@ package thewho.error
 
 sealed trait AppException extends Exception
 
-//
-//  REPOSITORY ERRORS
-//
-
-/**
- * An ADT equals to: RepositoryError | RepositoryFailure
- *
- * In scala 3 this will not be necessary any more.
- *
- * {{
- *   def findUser(id: UserId): IO[RepositoryFailure | UserNotFound, User] =
- *     sql"""SELECT * FROM users WHERE users.id = $id""".query[User].option.run
- *       .mapError(CommonRepositoryFailure)
- *       .flatMap{ case Some(user) => IO succeed user; case None => IO fail UserNotFound }
- * }}
- *
- */
-sealed trait RepositoryException extends AppException
-
 /**
  * A "recoverable" error: a kind of error that we would like to map to a status code more specific than a 500.
  */
-sealed trait RepositoryError extends RepositoryException
-
-sealed trait UserError       extends RepositoryError
-case object UserNotFound     extends UserError
-case object UserAlreadyExist extends UserError
-
-sealed trait CredentialError       extends RepositoryError
-case object CredentialNotFound     extends CredentialError
-case object CredentialAlreadyExist extends CredentialError
-case object NoCredentialsUpdated   extends CredentialError
-case object NoCredentialsCreated   extends CredentialError
+sealed trait AppError extends AppException
 
 /**
  * A "unrecoverable" failure: a kind of failure that we wouldn't want to handle, and we would let the http layer to
  * map to a 500.
  */
-sealed trait RepositoryFailure extends RepositoryException {
+sealed trait AppFailure extends AppException
+
+trait HasCause {
   def cause: Throwable
 }
 
-case class CommonRepositoryFailure(cause: Throwable) extends RepositoryFailure
-case class HeartBeatError(cause: Throwable)          extends RepositoryFailure
+//
+//  REPOSITORY ERRORS
+//
+
+sealed trait RepositoryException extends AppException
+
+sealed trait RepositoryError       extends AppError with RepositoryException
+case object UserNotFound           extends RepositoryError
+case object UserAlreadyExist       extends RepositoryError
+case object CredentialNotFound     extends RepositoryError
+case object CredentialAlreadyExist extends RepositoryError
+
+sealed trait RepositoryFailure                       extends AppFailure with RepositoryException
+case object NoCredentialsUpdated                     extends RepositoryFailure
+case object NoCredentialsCreated                     extends RepositoryFailure
+case class CommonRepositoryFailure(cause: Throwable) extends RepositoryFailure with HasCause
 
 //
 //  AUTH ERRORS
@@ -53,14 +40,11 @@ case class HeartBeatError(cause: Throwable)          extends RepositoryFailure
 
 sealed trait AuthException extends AppException
 
-sealed trait AuthError extends AuthException
+sealed trait AuthError extends AppError with AuthException
 case object Forbidden  extends AuthError
 
-sealed trait AuthFailure extends AuthException {
-  def cause: Throwable
-}
-
-case class JWTClaimFailure(cause: Throwable)    extends AuthFailure
-case class JWTEncodeFailure(cause: Throwable)   extends AuthFailure
-case class JWTDecodeFailure(cause: Throwable)   extends AuthFailure
-case class CirceDecodeFailure(cause: Throwable) extends AuthFailure
+sealed trait AuthFailure                        extends AppFailure with AuthException
+case class JWTClaimFailure(cause: Throwable)    extends AuthFailure with HasCause
+case class JWTEncodeFailure(cause: Throwable)   extends AuthFailure with HasCause
+case class JWTDecodeFailure(cause: Throwable)   extends AuthFailure with HasCause
+case class CirceDecodeFailure(cause: Throwable) extends AuthFailure with HasCause
--- src/main/scala/thewho/http/Routes.scala
+++ src/main/scala/thewho/http/Routes.scala
@@ -2,11 +2,11 @@ package thewho.http
 
 import cats.data.Kleisli
 import io.circe.generic.auto._
-import org.http4s.{ HttpRoutes, Request, Response }
-import org.http4s.implicits._
 import org.http4s.circe.CirceEntityDecoder._
 import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.implicits._
+import org.http4s.{ HttpRoutes, Request, Response }
 import scalaz.zio.interop.catz._
 import thewho.AppTask
 import thewho.auth._
@@ -16,16 +16,15 @@ object Routes {
   object dsl extends Http4sDsl[AppTask]
   import dsl._
 
-  // TODO #9 improve error handling
   val authRoutes: HttpRoutes[AppTask] = HttpRoutes.of[AppTask] {
     case req @ POST -> Root / "signup" =>
-      req decode [Credential] (signup(_).map(TokenResponse) >>= (Ok(_)))
+      req decode [Credential](signup(_).map(TokenResponse).foldM(ExceptionHandler.all, Created(_)))
     case req @ (GET | POST) -> Root / "login" =>
-      req decode [Credential] (login(_).map(TokenResponse) >>= (Ok(_)))
+      req decode [Credential](login(_).map(TokenResponse).foldM(ExceptionHandler.all, Ok(_)))
     case req @ POST -> Root / "change-password" =>
-      req decode [CredentialSecretUpdateForm] (changePassword(_).map(TokenResponse) >>= (Ok(_)))
+      req decode [CredentialSecretUpdateForm](changePassword(_).map(TokenResponse).foldM(ExceptionHandler.all, Ok(_)))
     case req @ POST -> Root / "signout" =>
-      req decode [Credential] (signout(_) >>= (Ok(_)))
+      req decode [Credential](signout(_).foldM(ExceptionHandler.all, Ok(_)))
   }
 
   val all: Kleisli[AppTask, Request[AppTask], Response[AppTask]] = authRoutes.orNotFound
